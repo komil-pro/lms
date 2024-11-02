b509f7def34d6695d43fc4523030cf9fb2ed2cfb
komil-pro <komil.tj@yandex.ru>
2024-11-02 13:39:24 +0500

    basic urls and template

--- xlms/students/views.py
+++ xlms/students/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from .models import Student
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    students = Student.objects.all()
+    template = loader.get_template("students/index.html")
+    context = {
+        "students": students,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    return render(request, "students/detail.html", {"student": student})
